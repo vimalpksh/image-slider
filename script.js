cc19aebf574ad0e400aab27c3fd5c85988b6d671
vimalpksh <itisvimal@gmail.com>
2023-02-16 23:56:19 +0530

    worked on button to move to next image and prev image.

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 const nextEl = document.querySelector(".next");
 const imageContEl = document.querySelector(".image-container");
 const imgsEl = document.querySelectorAll("img");
+const prevEl = document.querySelector(".prev");
 
 let currentImg = 1;
+
+const updateImg = function () {
+  if (currentImg > imgsEl.length) {
+    currentImg = 1;
+  } else if (currentImg < 1) {
+    currentImg = imgsEl.length;
+  }
+  imageContEl.style.transform = `translateX(-${(currentImg - 1) * 500}px)`;
+
+  setTimeout(() => {
+    currentImg++;
+    updateImg();
+  }, 3000);
+};
 
 nextEl.addEventListener("click", () => {
   currentImg++;
   updateImg();
 });
 
-const updateImg = function () {
-  imageContEl.style.transform = `translateX(-${(currentImg - 1) * 500}px)`;
-};
+prevEl.addEventListener("click", () => {
+  currentImg--;
+  updateImg();
+});
+
+updateImg();
